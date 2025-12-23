927336bb25fa13b4e41acda675026ff9e1f58002
haji ali <8277262677ali@gmail.com>
2025-12-24 01:32:38 +0530

    64497

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.generics import listapiview
+from.models import menucategoryserializer
+class menucategorylistview(listapiview):
+    queryset=menucategory.object.all()
+    serializer_class=menucategoryserializer
 
 # Create your views here.
