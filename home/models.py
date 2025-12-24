5db44964e6789a4aa77ab780e9b6b2550e134661
haji ali <8277262677ali@gmail.com>
2025-12-24 12:03:24 +0530

    64496

--- home/models.py
+++ home/models.py
@@ -3,3 +3,11 @@ from django.db import models
 # Create your models here.
 class orderstatus(models.model):
     name=models.charfeild(max_length=50,unique=True)
+    class coupon(models.model):
+        code=models.charfeild(max_length=50, unique=True)
+        discount_percentage=models.decimalfeild(max_digits=5, decimal_places=2)
+        is_active = models.booleanfeild(default=True)
+        valid_from=models.datefield()
+        valid_until=models.datefield()
+        def __str__(self):
+            return self.code
